14f6672c5faf39d5ea83cf47eeaa773fe7bb0af8
Viktor Kostadinov <viktor.pts@abv.bg>
2020-05-01 20:18:44 +0300

    shader exercise

---
+++ Assets/Scripts/PixelizationEffect.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PixelizationEffect : MonoBehaviour
+{
+    public Material effect;
+    void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        Graphics.Blit(source, destination, effect);
+    }
+}
